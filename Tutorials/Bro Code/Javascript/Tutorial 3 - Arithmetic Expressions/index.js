853e8aa2a773f18e3a8e486feba2b65d275349ac
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2023-07-29 17:18:00 +0800

    BroCode Tutorial 3

---
+++ Tutorials/Bro Code/Javascript/Tutorial 3 - Arithmetic Expressions/index.js
@@ -0,0 +1,8 @@
+// I already know these stuff... I touch them too much...
+
+let students = 49;
+let limit = 50;
+
+students = students > limit ? limit : students; // yea, no more than 50.
+
+console.log(students);
