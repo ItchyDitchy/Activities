61328b6fd2bd21eccfe4970cff4816f7237da326
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2023-09-20 11:57:43 +0800

    Exa1

--- C++/School/Module 1/Exa3.cpp
+++ C++/School/Module 1/Exa3.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
-#define ytd 2023
 
 using namespace std;
 
 int main() {
-    double num1, num2; cout << "Enter Num1: "; cin>>num1;
+    double num1, num2;
+    cout << "Enter Num1: "; cin>>num1;
     cout << "Enter Num2: "; cin>>num2;
-    cout<<"The sum is "<<num1+num2<<"\nThe diff is "<<num1-num2<<"\nThe quo is "<<num1/num2<<"\nThe prod is "<<num1*num2<<"\n";
+    cout<<"The sum is "<<num1+num2<<"\n";
+    cout<<"The diff is "<<num1-num2<<"\n";
+    cout<<"The quo is "<<num1/num2<<"\n";
+    cout<<"The prod is "<<num1*num2<<"\n";
     return 0;
 }
