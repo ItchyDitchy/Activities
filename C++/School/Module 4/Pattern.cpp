b5116b4815369370b6c084a5aeaf342a27d97864
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2023-10-10 05:10:33 +0800

    Removed Duplicates

--- C++/School/Module 4/Pattern.cpp
+++ C++/School/Module 4/Pattern.cpp
@@ -2,23 +2,11 @@
 #include <windows.h>
 using namespace std;
 
-int Amain() {
+main() {
     for (int x = 1; x <= 5; x++) {
         int y = 1;
         while (y++ <= x) {
             std::cout << "*";
-        }
-        std::cout << "\n";
-    }
-    return 0;
-}
-
-main() {
-    int x = 5, y = 1;
-    while (y <= 5) {
-        while (y <= x) {
-            std::cout << "*";
-            y++;
         }
         std::cout << "\n";
     }
