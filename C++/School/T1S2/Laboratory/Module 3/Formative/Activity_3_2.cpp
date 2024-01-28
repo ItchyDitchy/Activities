26d026a33254b16fc440df59bc5f5961e1439b96
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2024-01-28 16:33:47 +0800

    Formative 3 - Task 2

---
+++ C++/School/T1S2/Laboratory/Module 3/Formative/Activity_3_2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::cout << "*********************\n";
+    std::cout << "STRING COPY\n";
+    std::cout << "*********************\n";
+    char str1[100], str2[100];
+    std::cout << "Enter a first word (str1): ";
+    std::cin.getline(str1, sizeof(str1));
+    std::cout << "Enter a first word (str2): ";
+    std::cin.getline(str2, sizeof(str2));
+    strcpy(str1, str2);
+    std::cout << "New string value for str1: " << str1;
+    return 0;
+}
