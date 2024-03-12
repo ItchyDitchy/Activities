77803162333ea602025c7a7b72d76a8c8618ef7b
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2024-03-12 10:55:18 +0800

    String utils, fancy😎

---
+++ C++/School/T1S2/Laboratory/Module 6/Technical/StringUtils.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+using namespace std;
+
+string strip(string txt) {
+    string tmp = "";
+    for (char c : txt)
+        if (!isspace(c))
+            tmp += c;
+    return tmp;
+}
+
+string toLower(string txt) {
+    for (char &c : txt)
+        c = tolower(c);
+    return txt;
+}
+
+string toUpper(string txt) {
+    for (char &c : txt)
+        c = toupper(c);
+    return txt;
+}
+
+string titleCase(string txt) {
+    for (int i = 0; i < txt.size(); i++)
+        txt[i] = isspace(txt[i-1]) ? toupper(txt[i]) : tolower(txt[i]);
+    return txt;
+}
