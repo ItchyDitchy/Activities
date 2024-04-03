fb13373af75bf052383345d19272c4de3645cadb
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2024-04-03 15:30:35 +0800

    Delete main.py

--- Python/Activity 1 - Parallelogram/main.py
+++
@@ -1,3 +0,0 @@
-height = input("Enter height of your parallelogram? \n")
-base = input("Enter base of your parallelogram? \n")
-print("The area of your parallelogram is equal to " + str(int(height) * int(base)))
