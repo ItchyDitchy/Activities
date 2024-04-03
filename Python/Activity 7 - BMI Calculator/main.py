b992717fc438d1c2336a88433949b0e20fd9eab2
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2024-04-03 15:30:47 +0800

    Delete main.py

--- Python/Activity 7 - BMI Calculator/main.py
+++
@@ -1,9 +0,0 @@
-Height = input(" Please Enter your Height: ")
-Weight = input(" Please Enter the Weight: ")
-Age = input(" Please Enter the Age: ") #IDK where to use this to calculate BMI...
-parsedHeight = float(Height)
-parsedWeight = float(Weight)
-parsedAge = int(Age) # Just to give an error if they typed a random value.
-print("Your BMI is equal to " + str(parsedWeight / parsedHeight ** 2))
-print("Your age is also " + str(parsedAge) + " for no reason")
-#Also can use if and elif statements for checking if they are either underweight, normal, overweight, obese or extremely obese.
