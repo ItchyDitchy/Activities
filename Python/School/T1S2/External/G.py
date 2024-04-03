7111bc3668ce68d01167c8649c9ccea3c52f5a81
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2024-04-03 15:31:23 +0800

    Create G.py

---
+++ Python/School/T1S2/External/G.py
@@ -0,0 +1,11 @@
+# Code that produces errors:
+"""
+print('BY WILLIAM ERNEST HENLEY)
+prnt('In the fell clutch of circumstance')
+primt('     My head is bloody, but unbowed.')
+print'Beyond this place of wrath and tears')
+print
+     print('It matters not how strait the gate,')
+print(''I am the master of my fate,")
+Print('    I am the captain of my soul.')
+"""
