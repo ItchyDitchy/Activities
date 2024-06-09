7f3c1aba7477abfce2fddfb8471021be79140262
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2024-06-09 15:48:33 +0800

    Create Activity_6_2.cpp

---
+++ C++/School/T1S3/Laboratory/Module 6/Activity_6_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void towerOfHanoi(int num, char a = 'A', char b = 'B', char c = 'C') {
+  if (num == 1) {
+    cout << "Disk " << num << " moved from " << a << " to " << c << endl;
+    return;
+  }
+
+  towerOfHanoi(num - 1, a, c, b);
+  cout << "Disk " << num << " moved from " << a << " to " << c << endl;
+  towerOfHanoi(num - 1, b, a, c);
+}
+
+int main() {
+  int diskNum = -1;
+  while (2 > diskNum || diskNum > 3) {
+    cout << "Input: ";
+    cin >> diskNum;
+  }
+  towerOfHanoi(diskNum);
+  return 0;
+}
