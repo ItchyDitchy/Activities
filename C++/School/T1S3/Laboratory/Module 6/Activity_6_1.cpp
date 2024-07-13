4e9fb364c99b202dcde78c94733f5d6ee500676c
ItchyDitchy <51221502+ItchyDitchy@users.noreply.github.com>
2024-07-13 23:29:15 +0800

    Update Activity_6_1.cpp

--- C++/School/T1S3/Laboratory/Module 6/Activity_6_1.cpp
+++ C++/School/T1S3/Laboratory/Module 6/Activity_6_1.cpp
@@ -3,27 +3,22 @@
 #include <iostream>
 #include <unordered_map>
 
-using namespace std;
-
 void calcDuplicates(int cur, int arr[], int size,
-                    unordered_map<int, int>& count) {
-  if (arr[cur])
-    count[arr[cur]] += 1;
-  else
-    count[arr[cur]] = 1;
+                    std::unordered_map<int, int>& count) {
+  count[arr[cur]] = arr[cur] ? count[arr[cur]] + 1 : 1;
   if (cur < size - 1) {
     calcDuplicates(cur + 1, arr, size, count);
     return;
   }
   for (const auto& pair : count)
-    cout << pair.first << " appears " << pair.second << " time"
-         << (pair.second > 1 ? "s" : "") << endl;
+    std::cout << pair.first << " appears " << pair.second << " time"
+              << (pair.second > 1 ? "s" : "") << std::endl;
 }
 
 int main() {
   int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
-  unordered_map<int, int> count;
+  std::unordered_map<int, int> count;
   calcDuplicates(0, arr, sizeof(arr) / sizeof(arr[0]), count);
-  cout << clock();
+  std::cout << clock();
   return 0;
 }
